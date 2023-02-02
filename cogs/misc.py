8b7cd1a8f35834f4e105b9b1b8ae151103c7ffb7
Vinesh Benny <vinesh.benny@mail.utoronto.ca>
2023-02-02 16:10:11 -0500

    fixed

--- cogs/misc.py
+++ cogs/misc.py
@@ -9,6 +9,7 @@ import random
 import html
 import time
 import sqlite3
+import traceback
 
 
 class Miscellaneous(commands.Cog):
@@ -21,6 +22,62 @@ class Miscellaneous(commands.Cog):
         response = requests.get("https://api.kanye.rest")
         json_data = json.loads(response.text)
         return json_data["quote"]
+
+    ##################################################################################################
+    ###################################### GLOBAL ERROR HANDLER ######################################
+    ##################################################################################################
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error: commands.CommandError):
+        # This prevents any commands with local handlers being handled here in on_command_error.
+        if hasattr(ctx.command, "on_error"):
+            return
+
+        # This prevents any cogs with an overwritten cog_command_error being handled here.
+        cog = ctx.cog
+        if cog:
+            if cog._get_overridden_method(cog.cog_command_error) is not None:
+                return
+
+        ignored = ()
+
+        # Allows us to check for original exceptions raised and sent to CommandInvokeError.
+        # If nothing is found. We keep the exception passed to on_command_error.
+        error = getattr(error, "original", error)
+
+        # Anything in ignored will return and prevent anything happening.
+        if isinstance(error, ignored):
+            return
+
+        if isinstance(error, commands.DisabledCommand):
+            await ctx.send(f"{ctx.command} has been disabled.")
+
+        elif isinstance(error, commands.NoPrivateMessage):
+            try:
+                await ctx.author.send(
+                    f"{ctx.command} can not be used in Private Messages."
+                )
+            except discord.HTTPException:
+                pass
+
+        # For this error example we check to see where it came from...
+        elif isinstance(error, commands.BadArgument):
+            await ctx.send("Bad Argument provided.")
+
+        elif isinstance(error, commands.UserNotFound):
+            await ctx.send("No user found. Try again")
+
+        elif isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send("Missing required argument. Try again")
+
+        else:
+            # All other Errors not returned come here. And we can just print the default TraceBack.
+            print(
+                "Ignoring exception in command {}:".format(ctx.command), file=sys.stderr
+            )
+            traceback.print_exception(
+                type(error), error, error.__traceback__, file=sys.stderr
+            )
 
     @commands.command(
         name="hello",
@@ -98,7 +155,7 @@ class Miscellaneous(commands.Cog):
         help="Play a round of trivia. Choose a category with &trivia {category}. To view a list of categories use &trivia categories. If no category is specified, a random category will be chosen.",
     )
     async def trivia(self, ctx: commands.Context, category: str = ""):
-        if category == "leaderbord" or category == "lb":
+        if category == "leaderboard" or category == "lb":
             conn = sqlite3.connect("database.db")
             c = conn.cursor()
             sql = """
@@ -246,6 +303,7 @@ class Miscellaneous(commands.Cog):
             embed = discord.Embed(title="Duck", color=discord.Colour.random())
             embed.set_image(url=data["url"])
             await ctx.send(embed=embed)
+
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Miscellaneous(bot))
