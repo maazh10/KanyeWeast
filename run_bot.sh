c060605f9d7babf06ab44eb7debc1b775c571e1c
Vinesh Benny <vinesh.benny@mail.utoronto.ca>
2023-05-04 01:26:50 -0400

    made script dead simple

--- run_bot.sh
+++ run_bot.sh
@@ -2,15 +2,7 @@
 
 cd /home/server/KanyeWeast
 
-branch=$(git rev-parse --abbrev-ref HEAD)
+git fetch
+git reset --hard origin/$branch
 
-latest_commit=$(git rev-parse HEAD)
-
-remote_commit=$(git ls-remote $(git config --get remote.origin.url) refs/heads/$branch | awk '{ print $1 }')
-
-if [ $latest_commit != $remote_commit ] -o [ "$1" == "--force" ]; then
-    git fetch
-    git reset --hard origin/$branch
-
-    docker compose down && docker compose up -d --build
-fi
+docker compose down && docker compose up -d --build
